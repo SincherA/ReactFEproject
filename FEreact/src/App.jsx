5c3fb6a3d359f7e3c9aea9ffa7a60a1342a980af
Sincher Ahmedov <sincher.ahmedov@students.beaminstitute.org>
2023-09-26 11:21:32 +0200

    leftnavbar links setled

--- FEreact/src/App.jsx
+++ FEreact/src/App.jsx
@@ -1,16 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import './components/Navbar.css'
 import Navbar from './components/Navbar.jsx';
 import LeftNavbar from './components/LeftNavbar';
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+import Services from './components/Pages/Services';
+import Contact from './components/Pages/Contact';
 
 function App() {
 
   return (
+
     <div >
-      <Navbar />
-      <LeftNavbar />
-   </div>
-  )
+      <Router>
+        <Navbar />
+        <LeftNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        </Router >
+    </div>
+);
 }
 
 export default App
