d056659ffceabab9c2be913222e8955a7ae9fb0b
Sincher Ahmedov <sincher.ahmedov@students.beaminstitute.org>
2023-09-26 16:32:52 +0200

    new lay out with tailwind kit

--- FEreact/src/App.jsx
+++ FEreact/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import './components/Navbar.css'
 import Navbar from './components/Navbar.jsx';
-import LeftNavbar from './components/LeftNavbar';
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
 import Services from './components/Pages/Services';
 import Contact from './components/Pages/Contact';
+import Footer from './components/Footer';
+import SideBar from './components/SideBar';
 
 function App() {
 
@@ -15,7 +15,8 @@ function App() {
     <div >
       <Router>
         <Navbar />
-        <LeftNavbar />
+        <SideBar />
+        <Footer />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
